b86470c14898a630b97eeac3af1b1d491961be88
Paulo Lazarini <paulo_lazarini@hotmail.com>
2025-06-24 20:23:53 -0300

    vercel teste

--- api_root/settings.py
+++ api_root/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'api_root.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
