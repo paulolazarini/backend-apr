d5731bfa775f1a17afd3421820e1965017283e96
Paulo Lazarini <paulo_lazarini@hotmail.com>
2025-07-01 20:03:58 -0300

    vercel teste

--- api_root/settings.py
+++ api_root/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%^m_%t^+s$*wp0tb_gx=%!4ek+1+$*gh2h7e9ne$j+hp%(w%c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
